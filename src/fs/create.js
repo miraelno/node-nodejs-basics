e2ca5c569b30b4554019d1512152c04858654ef5
Karina Bohdanova <bogdanova.karina22@gmail.com>
2025-10-25 14:41:11 +0300

    feat: finish fs tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { FsOperationError } from './errors.js';
+
+
+const filePath = path.resolve('src', 'fs', 'files', 'fresh.txt');
+
 const create = async () => {
-  // Write your code here
+
+  try {
+    await fs.access(filePath);
+    throw new FsOperationError();
+  } catch (err) {
+    if (err && err.code === 'ENOENT') {
+      await fs.mkdir(path.dirname(filePath), { recursive: true });
+      await fs.writeFile(filePath, 'I am fresh and young');
+      return;
+    }
+    throw err;
+  }
 };
 
 await create();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { FsOperationError } from './errors.js';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filesDir = path.join(__dirname, 'files');
+
 const list = async () => {
-  // Write your code here
+  try {
+    console.log(await fs.readdir(filesDir));
+  } catch (err) {
+    throw new FsOperationError();
+  }
 };
 
 await list();
