4d0c2b2144d13a0c83f675774187d36e0b258084
Karina Bohdanova <bogdanova.karina22@gmail.com>
2025-10-26 22:57:05 +0200

    feat: finish cli and modules tasks

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,12 +1,19 @@
-const path = require('node:path');
-const { release, version } = require('node:os');
-const { createServer: createServerHttp } = require('node:http');
+import path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import { fileURLToPath } from 'url';
 
-require('./files/c.cjs');
+import('./files/c.cjs');
 
 const random = Math.random();
 
-const unknownObject = random > 0.5 ? require('./files/a.json') : require('./files/b.json');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const unknownObject = random > 0.5
+  ? (await import('./files/a.json', { with: { type: 'json' } }))
+  : (await import('./files/b.json', { with: { type: 'json' } }));
+
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
@@ -28,7 +35,4 @@ myServer.listen(PORT, () => {
   console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-  unknownObject,
-  myServer,
-};
+export { unknownObject, myServer, };
