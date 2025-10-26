ebde3581c9baad05769b45eeabf59e41b29b49dd
Karina Bohdanova <bogdanova.karina22@gmail.com>
2025-10-26 23:59:53 +0200

    feat: finish cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const childProcessFilePath = path.join(__dirname, 'files', 'script.js');
+  const childProcess = fork(childProcessFilePath, [...args], { silent: true });
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
