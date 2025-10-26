fc734a50faa29a1e81575964e511e105774d4b47
Karina Bohdanova <bogdanova.karina22@gmail.com>
2025-10-26 23:48:19 +0200

    feat: finish workers task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-  // Write your code here
+  const CPU_COUNT = os.cpus().length;
+  const tasks = [];
+  const results = [];
+
+  for (let i = 0; i < CPU_COUNT; i += 1) {
+    const workerPath = new URL('./worker.js', import.meta.url);
+    const worker = new Worker(workerPath, { workerData: 10 + i });
+
+    worker.on('message', (result) => {
+      results.push({ status: 'resolved', data: result });
+    })
+
+    worker.on('error', (error) => {
+      console.log(error)
+      results.push({ status: 'error', data: null });
+    })
+
+    tasks.push(worker);
+
+  }
+
+  await Promise.allSettled(
+    tasks.map((worker) => new Promise((res) => worker.on('exit', res)))
+  )
+
+  console.log(results);
 };
 
 await performCalculations();
